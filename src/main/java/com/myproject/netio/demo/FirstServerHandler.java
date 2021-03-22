40e4a013add7d8c35aebd54a4089fe8211efdb3f
xiaoxing.diao <xiaoxing.diao@newtouch.com>
2021-03-22 16:07:08 +0800

    双向通讯

---
+++ src/main/java/com/myproject/netio/demo/FirstServerHandler.java
@@ -0,0 +1,22 @@
+package com.myproject.netio.demo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+public class FirstServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 接收到消息后调用：服务端读取数据
+     * @param ctx
+     * @param msg
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+    }
+}
--- src/main/java/com/myproject/netio/demo/NettyServer.java
+++ src/main/java/com/myproject/netio/demo/NettyServer.java
@@ -3,6 +3,7 @@ package com.myproject.netio.demo;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -12,6 +13,8 @@ import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 
 public class NettyServer {
+    private static final int PORT = 8000;
+
     public static void main(String[] args) {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
 
@@ -20,19 +23,23 @@ public class NettyServer {
         serverBootstrap
                 .group(boss, worker)//boss表示监听链接线程组、worker表示处理每一条链接的数据逻辑的线程组
                 .channel(NioServerSocketChannel.class)//指定IO模型
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {//指定每条数据的读写逻辑
-                        ch.pipeline().addLast(new StringDecoder());
-                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
-                            @Override
-                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
-                                System.out.println(msg);
-                            }
-                        });
+//                        ch.pipeline().addLast(new StringDecoder());
+//                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+//                            @Override
+//                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+//                                System.out.println(msg);
+//                            }
+//                        });
+                        ch.pipeline().addLast(new FirstServerHandler());
                     }
                 });
 //                .bind(8000);//绑定8000端口
-        bind(serverBootstrap,1000);
+        bind(serverBootstrap,PORT);
     }
 
 
