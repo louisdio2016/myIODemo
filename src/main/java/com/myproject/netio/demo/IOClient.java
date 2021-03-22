6e887bf4bd3a6d95c2ac5925519f45b1ae66c8db
xiaoxing.diao <xiaoxing.diao@newtouch.com>
2021-03-22 09:59:39 +0800

    创建socketDemo

---
+++ src/main/java/com/myproject/netio/demo/IOClient.java
@@ -0,0 +1,23 @@
+package com.myproject.netio.demo;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+public class IOClient {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello world 1").getBytes());
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                    }
+                }
+            } catch (IOException e) {
+            }
+        }).start();
+    }
+}
